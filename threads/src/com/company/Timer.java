63c59526d547b38993f7de6d68029a0cfe126001
Kabe KUŞ <kabekus.123@gmail.com>
2021-12-02 00:42:40 +0300

    Race of Threads

---
+++ threads/src/com/company/Timer.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Timer implements Runnable{
+    int index;
+
+    public Timer() {
+        this.index = 0;
+    }
+
+    @Override
+    public synchronized void run() {
+        int number = Main.numbers.get(index);
+
+        if (number % 2 == 0)
+            Main.even.add(number);
+        else
+            Main.odd.add(number);
+
+        System.out.println("İş bitti. "
+                + Thread.currentThread().getName()
+                + " Even Size:" + Main.even.size()
+                + " Odd Size: " + Main.odd.size());
+        index++;
+    }
+}
